4e013226374376268c89941614d5a2208ee54792
Airton <airton.ddk@hotmail.com>
2019-02-11 23:50:57 -0300

    Commit inicial, criando projeto com as dependencias

---
+++ src/main/java/br/com/airton/Springrestjwth2Application.java
@@ -0,0 +1,14 @@
+package br.com.airton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrestjwth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrestjwth2Application.class, args);
+	}
+
+}
+
