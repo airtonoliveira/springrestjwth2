88f8c9857af55a338ca2a0263040c58f7e7da6ea
Airton Oliveira <airton.ddk@hotmail.com>
2019-04-02 07:34:11 -0300

    Atualizando

--- src/main/java/br/com/airton/resources/CategoriaResource.java
+++ src/main/java/br/com/airton/resources/CategoriaResource.java
@@ -1,16 +1,21 @@
 package br.com.airton.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.airton.domain.Categoria;
+import br.com.airton.dto.CategoriaDTO;
 import br.com.airton.services.CategoriaService;
 
 @RestController
@@ -18,27 +23,30 @@ import br.com.airton.services.CategoriaService;
 public class CategoriaResource {
 	
 	@Autowired
-	private CategoriaService categoriaService;
+	private CategoriaService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		//return ResponseEntity.ok().body(categoriaService.listarTodas());
+		return null;
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		
-		Categoria categoria = categoriaService.buscar(id);
-		
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {		
+		Categoria categoria = service.find(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		//Seguindo protocolo HTTP a requisição deve retornar a URI e o status adequado.
+		//https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
